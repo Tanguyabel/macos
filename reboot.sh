cdbe774d5679ac4e94e5470d27f1ee5375c0b65e
Tanguy Abel <tanguy.abel@gmail.com>
2020-03-08 20:29:37 +0100

    Create reboot.sh

---
+++ reboot.sh
@@ -0,0 +1,5 @@
+# always show hidden files in the finder
+defaults write com.apple.finder AppleShowAllFiles YES
+
+# install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
